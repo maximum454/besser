49b1e8aff58968c3cee677531f9e7885720d0f1a
maxim <karachyov@musan.kz>
2017-11-20 10:30:10 +0600

    Merge branches 'master' and 'radion' of https://bitbucket.org/maximum454/besser
    # Conflicts:
    #	build/catalog-1.html
    #	build/catalog-2.html
    #	build/css/main.css
    #	build/img/slider-img.png
    #	build/js/main.js

--- src/js/partials/cal.js
+++ src/js/partials/cal.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var inputs = document.querySelectorAll('.form input');
     for (i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener('keyup', function(e) {
-
+            if(!$(inputs).val() == ''){
                 var lr = $('#len-room').val();
                 var lw = $('#width-room').val();
                 var lb = $('#len-board').val() / 1000;
@@ -14,7 +14,9 @@ $(document).ready(function(){
                 var formula = ((lr*lw))/(lb*wb*n);
                 var pin = Math.ceil(formula) * lprice;
                 $('#spinner-value').html(pin);
-            
+            }else{
+
+            }
         });
     }
 });
