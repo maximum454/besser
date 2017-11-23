f5678790adfea71e96c85e7befb7a0c9ccfd2375
Maxim <maximum454@gmail.com>
2017-11-23 21:07:53 +0600

    скрипт калькулятора

--- src/js/partials/cal.js
+++ src/js/partials/cal.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
     var inputs = document.querySelectorAll('.form input');
     for (i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener('keyup', function(e) {
-
                 var lr = $('#len-room').val();
                 var lw = $('#width-room').val();
                 var lb = $('#len-board').val() / 1000;
@@ -14,7 +13,7 @@ $(document).ready(function(){
                 var formula = ((lr*lw))/(lb*wb*n);
                 var pin = Math.ceil(formula) * lprice;
                 $('#spinner-value').html(pin);
-            
+                $('#number-packaging').val(Math.ceil(formula));
         });
     }
 });
