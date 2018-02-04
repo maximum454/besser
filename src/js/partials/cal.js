e9826c4e9e6b68fc7a294f5dbe496a821cf7f133
Maxim <maximum454@gmail.com>
2018-02-04 21:39:16 +0600

    Добавил  общую квадратуру в калькулятор

--- src/js/partials/cal.js
+++ src/js/partials/cal.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-
-
     var inputs = document.querySelectorAll('.form input');
     for (i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener('keyup', function(e) {
@@ -9,10 +7,11 @@ $(document).ready(function(){
                 var lb = $('#len-board').val() / 1000;
                 var wb = $('#width-board').val() / 1000;
                 var n = $('#number-board').val();
+                var itogo = (lr*lw);
                 var lprice = $('#lprice').text();
                 var formula = ((lr*lw))/(lb*wb*n);
                 var pin = Math.ceil(formula) * lprice;
-                $('#spinner-value').html(pin);
+                $('#spinner-value').html('<br>'+itogo+'м<sup>2</sup> = '+pin);
                 $('#number-packaging').val(Math.ceil(formula));
         });
     }
