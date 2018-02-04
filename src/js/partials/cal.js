f2d48fe83edfa5226506a1a0d5983116e4829b34
Maxim <maximum454@gmail.com>
2018-02-04 21:51:34 +0600

    fix

--- src/js/partials/cal.js
+++ src/js/partials/cal.js
@@ -7,11 +7,12 @@ $(document).ready(function(){
                 var lb = $('#len-board').val() / 1000;
                 var wb = $('#width-board').val() / 1000;
                 var n = $('#number-board').val();
-                var itogo = (lr*lw);
                 var lprice = $('#lprice').text();
                 var formula = ((lr*lw))/(lb*wb*n);
+                var itogo = (lr*lw);
+                var price = itogo*lprice;
                 var pin = Math.ceil(formula) * lprice;
-                $('#spinner-value').html('<br>'+itogo+'м<sup>2</sup> = '+pin);
+                $('#spinner-value').html('<br>'+itogo+'м<sup>2</sup> = '+price);
                 $('#number-packaging').val(Math.ceil(formula));
         });
     }
